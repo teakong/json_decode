d040eda6ea6d39ad1a09b0421a9253ab3f22a7c5
teakong <2602945904@qq.com>
2015-09-10 23:28:19 +0800

    Create json_encode_cn.php
    从thinkphp官网看到的，测试通过。

---
+++ json_encode_cn.php
@@ -0,0 +1,49 @@
+<?php
+/**
+*@功能：json_encode不编译中文成unicode,看起来更容易理解
+*@参数：类型为array，$array是
+*@返回：类型为string
+ */
+function json_encode_cn($array)
+{
+	arrayRecursive($array, 'urlencode', true);
+	$json = json_encode($array);
+	return urldecode($json);
+}
+
+
+/**************************************************************
+   *
+   *    使用特定function对数组中所有元素做处理
+   *    @param  string  &$array     要处理的字符串
+   *    @param  string  $function   要执行的函数
+   *    @return boolean $apply_to_keys_also     是否也应用到key上
+   *    @access public
+   *
+ *************************************************************/
+function arrayRecursive(&$array, $function, $apply_to_keys_also = false)
+{
+	static $recursive_counter = 0;
+	if (++$recursive_counter > 1000) {
+		die('possible deep recursion attack');
+	}
+	if(!empty($array))
+	{
+		foreach ($array as $key => $value) {
+			if (is_array($value)) {
+				arrayRecursive($array[$key], $function, $apply_to_keys_also);
+			} else {
+				$array[$key] = $function($value);
+			}
+	 
+			if ($apply_to_keys_also && is_string($key)) {
+				$new_key = $function($key);
+				if ($new_key != $key) {
+					$array[$new_key] = $array[$key];
+					unset($array[$key]);
+				}
+			}
+		}
+	}
+	$recursive_counter--;
+}
